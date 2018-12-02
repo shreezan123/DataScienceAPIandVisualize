796a3195d4bb3d2b078a27da5ce0866879f12d77
Shrijan Aryal <shrijan.aryal123@gmail.com>
2018-12-02 01:19:43 -0500

    main functions

--- visualize.py
+++ visualize.py
@@ -5,10 +5,13 @@ first_criteria = df['year'].map(lambda x: x >= 2009)
 second_criteria = df['year'].map(lambda x:x>=1989 and x<=2002)
 new_df = df[first_criteria]
 old_df = df[second_criteria]
-
 new_df['Value'] = new_df['Value'].str.replace(',', '').astype(float)
 old_df['Value'] = old_df['Value'].str.replace(',', '').astype(float)
 
-new_df.plot.line(x = 'reference_period_desc',y = 'Value',title = '2009 and beyond')
-old_df.plot.line(x = 'reference_period_desc',y = 'Value',title = '1989 to 2002')
-plt.show()
+def createPlots():
+    new_df.plot.line(x = 'reference_period_desc',y = 'Value',title = '2009 and beyond')
+    old_df.plot.line(x = 'reference_period_desc',y = 'Value',title = '1989 to 2002')
+    plt.show()
+
+if __name__ == '__main__':
+    createPlots()
